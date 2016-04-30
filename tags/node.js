605e6a46febf105cc9d2f17a7ca171159b496bb9
Mike Mazur <mmazur@gmail.com>
2016-05-01 06:54:20 +0800

    auto commit from delicious import beta test 2016-05-01T06:54:20Z

--- tags/node.js
+++ tags/node.js
@@ -4,4 +4,10 @@
     "hash": "2b4464c073098490aac48db3208ad892", 
     "uri": "http://blog.fogcreek.com/the-trello-tech-stack/", 
     "title": "[No Title]"
+}{
+    "ver": "gitmark.0.2", 
+    "creator": "delicious:mike@yeti", 
+    "hash": "af43206df60fb25053310fee37fb84f4", 
+    "uri": "http://blip.tv/file/2899135/", 
+    "title": "[No Title]"
 }
